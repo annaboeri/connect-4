d64a1995d616ce298e8757f1ce7d4d99fa056138
Anna Boeri <annaboeri@gmail.com>
2018-02-06 14:17:09 -0800

    updated CSS and began writing functionality to make disk appear to slide down column

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
 var game = {
-    players: [{name: "player1", color: "red"}, {name: "player2", color: "yellow"}],
+    players: [{name: "Player 1", color: "red"}, {name: "Player 2", color: "yellow"}],
     currentPlayer: null,
     init: function(){
         console.log('Game initialized')
         game.currentPlayer = game.players[0]
-
     },
+    resetGame: function(){
+        console.log("GAME OVER")
+        game.init()
+        $row.css("background-color", "rgb(255, 255, 255)")
+    },
+
     switchPlayer: function(){
         if (game.currentPlayer === game.players[0]){
             game.currentPlayer = game.players[1]
@@ -14,10 +19,22 @@ var game = {
             game.currentPlayer = game.players[0]
         }
      },
+    
     renderCurrentPlayer: function(){
-        for (i = 5; i >= 0; i--){
+
+        for (var i = 5; i >= 0; i--){
             var $newSlot = $(this).children().eq(i)
+            console.log($(this).children())
+           // console.log($newSlot)
             if ($newSlot.css("background-color") === "rgb(255, 255, 255)"){
+               var $previousSlots = $newSlot.prevAll()
+                console.log($newSlot.eq())
+                for (var j = 0; j < i; j++){
+                    var $otherSlot = $(this).children().eq(j)
+                    $otherSlot.css("background-color", "pink")
+                   // setTimout( "$otherSlot.css('background-color', 'rgb(255, 255, 255)')" , 1000)
+
+                }    
                 $newSlot.css("background-color", game.currentPlayer.color)
                 break
             }
@@ -28,9 +45,10 @@ var game = {
         game.checkRightAndDown()
         game.checkRightAndUp()
         game.switchPlayer()
-     },
+    },
+
     updateScore: function(){
-        if (game.currentPlayer.name === "player1"){
+        if (game.currentPlayer.name === "Player 1"){
             currentPlayer1Score = currentPlayer1Score + 1
             $player1Score.text(currentPlayer1Score) 
         } else {
@@ -46,11 +64,11 @@ var game = {
             $currentSlot.css("background-color") === $currentSlot.next().css("background-color") &&
             $currentSlot.next().css("background-color") === $currentSlot.next().next().css("background-color") &&
             $currentSlot.next().next().css("background-color") === $currentSlot.next().next().next().css("background-color")){
-                alert(game.currentPlayer.name + " ,you won!")
+                $gameResultOutput.css("color", game.currentPlayer.color)
+                $gameResultOutput.text(game.currentPlayer.name + " wins!")
                 game.updateScore()
             }
         }
- 
     },
     
     checkHorizontal: function(){
@@ -82,7 +100,6 @@ var game = {
             game.updateScore()
             } 
         }
-     
     },
 
 
@@ -99,13 +116,20 @@ var game = {
             game.updateScore()
             }
        }
-  
     } 
 }
     
 game.init()
 
 var $currentSlot 
+var $newGame = $('#new-game')
+var $player1Score = $('#player-1-score')
+var $player2Score = $('#player-2-score')
+var $gameResultOutput = $('#game-result-output')
+var $row = $('.row')
+
+var currentPlayer1Score = 0
+var currentPlayer2Score = 0
 
 var $column1 = $('#column-1')
 var $column2 = $('#column-2')
@@ -115,6 +139,7 @@ var $column5 = $('#column-5')
 var $column6 = $('#column-6')
 var $column7 = $('#column-7')
 
+
 var $row1 = $('.row-1')
 var $row2 = $('.row-2')
 var $row3 = $('.row-3')
@@ -122,12 +147,6 @@ var $row4 = $('.row-4')
 var $row5 = $('.row-5')
 var $row6 = $('.row-6')
 var $row7 = $('.row-7')
-
-var $player1Score = $('#player-1-score')
-var $player2Score = $('#player-2-score')
-
-var currentPlayer1Score = 0
-var currentPlayer2Score = 0
 
 
 $($column1).click(game.renderCurrentPlayer)
@@ -137,3 +156,5 @@ $($column4).click(game.renderCurrentPlayer)
 $($column5).click(game.renderCurrentPlayer)
 $($column6).click(game.renderCurrentPlayer)
 $($column7).click(game.renderCurrentPlayer)
+
+$($newGame).click(game.resetGame)
