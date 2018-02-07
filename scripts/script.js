be16b07f70610c408fecad1988d646f076df7d8f
Anna Boeri <annaboeri@gmail.com>
2018-02-06 18:49:41 -0800

    mvp accomplished

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,41 @@
+
+var $flexContainer = $('.flex-container')
+var $gameResultOutput = $('#game-result-output')
+var $newGame = $('#new-game')
+var $player1Score = $('#player-1-score')
+var $player2Score = $('#player-2-score')
+var $row = $('.row')
+var $currentSlot 
+var currentPlayer1Score = 0
+var currentPlayer2Score = 0
+
+var $row1 = $('.row-1')
+var $row2 = $('.row-2')
+var $row3 = $('.row-3')
+var $row4 = $('.row-4')
+var $row5 = $('.row-5')
+var $row6 = $('.row-6')
+var $row7 = $('.row-7')
+
+//highlight players whose turn it is
+
+
 var game = {
     players: [{name: "Player 1", color: "red"}, {name: "Player 2", color: "yellow"}],
     currentPlayer: null,
+    gameOver: false,
     init: function(){
         console.log('Game initialized')
         game.currentPlayer = game.players[0]
-    },
-    resetGame: function(){
-        console.log("GAME OVER")
-        game.init()
+        game.gameOver = false
+        $gameResultOutput.text("")
         $row.css("background-color", "rgb(255, 255, 255)")
     },
+     //resetGame: function(){
+    //     console.log("GAME OVER")
+    //     game.init()
+    
+    // },
 
     switchPlayer: function(){
         if (game.currentPlayer === game.players[0]){
@@ -21,31 +47,22 @@ var game = {
      },
     
     renderCurrentPlayer: function(){
-
-        for (var i = 5; i >= 0; i--){
+        if (game.gameOver !== true){
+            for (var i = 5; i >= 0; i--){
             var $newSlot = $(this).children().eq(i)
-            console.log($(this).children())
-           // console.log($newSlot)
             if ($newSlot.css("background-color") === "rgb(255, 255, 255)"){
-               var $previousSlots = $newSlot.prevAll()
-                console.log($newSlot.eq())
-                for (var j = 0; j < i; j++){
-                    var $otherSlot = $(this).children().eq(j)
-                    $otherSlot.css("background-color", "pink")
-                   // setTimout( "$otherSlot.css('background-color', 'rgb(255, 255, 255)')" , 1000)
-
+                   $newSlot.css("background-color", game.currentPlayer.color)
+                   break
                 }    
-                $newSlot.css("background-color", game.currentPlayer.color)
-                break
-            }
-        }   
-        $currentSlot = $newSlot
-        game.checkVertical()
-        game.checkHorizontal()
-        game.checkRightAndDown()
-        game.checkRightAndUp()
-        game.switchPlayer()
-    },
+            }   
+            $currentSlot = $newSlot
+            game.checkVertical()
+            game.checkHorizontal()
+            game.checkRightAndDown()
+            game.checkRightAndUp()
+            game.switchPlayer()
+        }    
+},
 
     updateScore: function(){
         if (game.currentPlayer.name === "Player 1"){
@@ -67,6 +84,9 @@ var game = {
                 $gameResultOutput.css("color", game.currentPlayer.color)
                 $gameResultOutput.text(game.currentPlayer.name + " wins!")
                 game.updateScore()
+                game.gameOver = true
+                //document.querySelectorAll(".flex-container").removeEventListener("click", game.renderCurrentPlayer)
+
             }
         }
     },
@@ -80,12 +100,12 @@ var game = {
             $currentSlot.css("background-color") === $currentSlot.parent().next().children().eq(currentSlotIndex).css("background-color") &&
             $currentSlot.parent().next().children().eq(currentSlotIndex).css("background-color") === $currentSlot.parent().next().next().children().eq(currentSlotIndex).css("background-color") &&
             $currentSlot.parent().next().next().children().eq(currentSlotIndex).css("background-color")  === $currentSlot.parent().next().next().next().children().eq(currentSlotIndex).css("background-color")) {
-            alert(game.currentPlayer.name + " ,you won!")
+            $gameResultOutput.css("color", game.currentPlayer.color)
+            $gameResultOutput.text(game.currentPlayer.name + " wins!")
             game.updateScore()
             } 
         }
     },
-
 
     checkRightAndDown: function(){
         var currentSlotIndex = $currentSlot.index()
@@ -96,12 +116,12 @@ var game = {
             $currentSlot.css("background-color") === $currentSlot.parent().next().children().eq(currentSlotIndex + 1).css("background-color") &&
             $currentSlot.parent().next().children().eq(currentSlotIndex + 1).css("background-color") === $currentSlot.parent().next().next().children().eq(currentSlotIndex + 2).css("background-color") &&
             $currentSlot.parent().next().next().children().eq(currentSlotIndex + 2).css("background-color")  === $currentSlot.parent().next().next().next().children().eq(currentSlotIndex + 3).css("background-color")) {
-            alert(game.currentPlayer.name + " ,you won!")
+            $gameResultOutput.css("color", game.currentPlayer.color)
+            $gameResultOutput.text(game.currentPlayer.name + " wins!")
             game.updateScore()
             } 
         }
     },
-
 
     checkRightAndUp: function(){
        var currentSlotIndex = $currentSlot.index()
@@ -112,49 +132,23 @@ var game = {
             $currentSlot.css("background-color") === $currentSlot.parent().next().children().eq(currentSlotIndex - 1).css("background-color") &&
             $currentSlot.parent().next().children().eq(currentSlotIndex - 1).css("background-color") === $currentSlot.parent().next().next().children().eq(currentSlotIndex - 2).css("background-color") &&
             $currentSlot.parent().next().next().children().eq(currentSlotIndex - 2).css("background-color")  === $currentSlot.parent().next().next().next().children().eq(currentSlotIndex - 3).css("background-color")) {
-            alert(game.currentPlayer.name + " ,you won!")
+            $gameResultOutput.css("color", game.currentPlayer.color)
+            $gameResultOutput.text(game.currentPlayer.name + " wins!")
             game.updateScore()
             }
        }
     } 
 }
-    
+
+document.querySelector("#column-1").addEventListener("click", game.renderCurrentPlayer)
+document.querySelector("#column-2").addEventListener("click", game.renderCurrentPlayer)
+document.querySelector("#column-3").addEventListener("click", game.renderCurrentPlayer)
+document.querySelector("#column-4").addEventListener("click", game.renderCurrentPlayer)
+document.querySelector("#column-5").addEventListener("click", game.renderCurrentPlayer)
+document.querySelector("#column-6").addEventListener("click", game.renderCurrentPlayer)
+document.querySelector("#column-7").addEventListener("click", game.renderCurrentPlayer)
+
+
 game.init()
 
-var $currentSlot 
-var $newGame = $('#new-game')
-var $player1Score = $('#player-1-score')
-var $player2Score = $('#player-2-score')
-var $gameResultOutput = $('#game-result-output')
-var $row = $('.row')
-
-var currentPlayer1Score = 0
-var currentPlayer2Score = 0
-
-var $column1 = $('#column-1')
-var $column2 = $('#column-2')
-var $column3 = $('#column-3')
-var $column4 = $('#column-4')
-var $column5 = $('#column-5')
-var $column6 = $('#column-6')
-var $column7 = $('#column-7')
-
-
-var $row1 = $('.row-1')
-var $row2 = $('.row-2')
-var $row3 = $('.row-3')
-var $row4 = $('.row-4')
-var $row5 = $('.row-5')
-var $row6 = $('.row-6')
-var $row7 = $('.row-7')
-
-
-$($column1).click(game.renderCurrentPlayer)
-$($column2).click(game.renderCurrentPlayer)
-$($column3).click(game.renderCurrentPlayer)
-$($column4).click(game.renderCurrentPlayer)
-$($column5).click(game.renderCurrentPlayer)
-$($column6).click(game.renderCurrentPlayer)
-$($column7).click(game.renderCurrentPlayer)
-
-$($newGame).click(game.resetGame)
+$($newGame).click(game.init)
